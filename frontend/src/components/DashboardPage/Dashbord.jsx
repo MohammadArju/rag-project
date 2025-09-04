0abcdbf7304c19275acc6780d832ada60d47b1d9
Mohammad Arju <mudassiransari8322@example.com>
2025-09-04 16:08:16 +0530

    Fix import paths for DashboardPage

--- frontend/src/components/DashboardPage/Dashbord.jsx
+++ frontend/src/components/DashboardPage/Dashbord.jsx
@@ -1,16 +1,18 @@
 import DeshNavbar from "./DashNavbar";
-import Left from "./leftSide/left";
+import Left from "./leftSide/Left";
 import Right from "./rightSide/Right";
+
 const Dashboard = () => {
   return (
     <>
       <DeshNavbar />
-      <div className="flex w-full h-screen dark:bg-gray-800 fixed ">
-        <Left></Left>
-        <Right></Right>
+      <div className="flex w-full h-screen dark:bg-gray-800 fixed">
+        <Left />
+        <Right />
       </div>
     </>
   );
 };
 
 export default Dashboard;
+
